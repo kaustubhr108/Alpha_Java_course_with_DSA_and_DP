87248b4aa80a5fdf03d9fbc00bee24e688e456b6
Kaustubh R Srivastav <kaustubhranjan7@gmail.com>
2024-05-05 23:03:13 +0530

    added program K5 for 19. RecursionBasics Module

--- P19_RecursionBasics/K5.java
+++ P19_RecursionBasics/K5.java
@@ -1,4 +1,16 @@
 //Factorial of N using RECURSION
 public class K5 {
-    
+    public static void main(String[] args) {
+    int a=5;
+    System.out.println("Factorail : "+factorial(a));
+    }
+
+    public static int factorial(int n) {
+        if(n==0){
+            return 1;
+        }
+//      int fnm1=factorial(n-1); finding the factorial of (n-1)
+        int fn=n*factorial(n-1);
+        return fn;
+    }
 }
